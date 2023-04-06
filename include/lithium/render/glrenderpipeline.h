dac1c6531c9a377b04a56456ba2c34cf63750692
filipfur <filipfur@live.se>
2023-04-06 15:54:27 +0200

    created basic opengl rendering function wrappings for ease of use

--- include/lithium/render/glrenderpipeline.h
+++ include/lithium/render/glrenderpipeline.h
@@ -77,6 +77,188 @@ namespace lithium
         }
 
     protected:
+        RenderPipeline* enableDepthTesting()
+        {
+            glEnable(GL_DEPTH_TEST);
+            return this;
+        }
+
+        RenderPipeline* disableDepthTesting()
+        {
+            glDisable(GL_DEPTH_TEST);
+            return this;
+        }
+
+        RenderPipeline* enableDepthWriting()
+        {
+            glDepthMask(GL_TRUE);
+            return this;
+        }
+
+        RenderPipeline* disableDepthWriting()
+        {
+            glDepthMask(GL_FALSE);
+            return this;
+        }
+
+        RenderPipeline* depthFunc(GLenum func)
+        {
+            glDepthFunc(func);
+            return this;
+        }
+
+        RenderPipeline* enableBlending()
+        {
+            glEnable(GL_BLEND);
+            return this;
+        }
+
+        RenderPipeline* disableBlending()
+        {
+            glDisable(GL_BLEND);
+            return this;
+        }
+
+        RenderPipeline* blendFunc(GLenum src=GL_SRC_ALPHA, GLenum dst=GL_ONE_MINUS_SRC_ALPHA)
+        {
+            glBlendFunc(src, dst);
+            return this;
+        }
+
+        RenderPipeline* enableFaceCulling()
+        {
+            glEnable(GL_CULL_FACE);
+            return this;
+        }
+
+        RenderPipeline* disableCulling()
+        {
+            glDisable(GL_CULL_FACE);
+            return this;
+        }
+
+        RenderPipeline* cullFrontFace()
+        {
+            glCullFace(GL_FRONT);
+            return this;
+        }
+
+        RenderPipeline* cullBackFace()
+        {
+            glCullFace(GL_BACK);
+            return this;
+        }
+
+        RenderPipeline* cullFrontAndBackFaces()
+        {
+            glCullFace(GL_FRONT_AND_BACK);
+            return this;
+        }
+
+        RenderPipeline* enableStencilTesting()
+        {
+            glEnable(GL_STENCIL_TEST);
+            return this;
+        }
+
+        RenderPipeline* disableStencilTesting()
+        {
+            glDisable(GL_STENCIL_TEST);
+            return this;
+        }
+
+        RenderPipeline* stencilFunc(GLenum func, GLint ref, GLuint mask=0xFF)
+        {
+            glStencilFunc(func, ref, mask);
+            return this;
+        }
+
+        RenderPipeline* stencilOp(GLenum sfail=GL_KEEP, GLenum dpfail=GL_KEEP, GLenum dppass=GL_REPLACE)
+        {
+            glStencilOp(sfail, dpfail, dppass);
+            return this;
+        }
+
+        RenderPipeline* stencilMaskSeparate(GLenum face, GLuint mask)
+        {
+            glStencilMaskSeparate(face, mask);
+            return this;
+        }
+
+        RenderPipeline* stencilFuncSeparate(GLenum face, GLenum func, GLint ref, GLuint mask)
+        {
+            glStencilFuncSeparate(face, func, ref, mask);
+            return this;
+        }
+
+        RenderPipeline* stencilOpSeparate(GLenum face, GLenum sfail=GL_KEEP, GLenum dpfail=GL_KEEP, GLenum dppass=GL_REPLACE)
+        {
+            glStencilOpSeparate(face, sfail, dpfail, dppass);
+            return this;
+        }
+
+        RenderPipeline* stencilMask(GLuint mask)
+        {
+            glStencilMask(mask);
+            return this;
+        }
+
+        RenderPipeline* enableStencilWriting(GLuint mask=0xFF)
+        {
+            return stencilMask(mask);
+        }
+
+        RenderPipeline* disableStencilWriting()
+        {
+            return stencilMask(0x00);
+        }
+
+        RenderPipeline* clearColor(float r=0.0f, float g=0.0f, float b=0.0f, float a=0.0f)
+        {
+            glClearColor(r, g, b, a);
+            return this;
+        }
+
+        RenderPipeline* clearDepth(float depth=1.0f)
+        {
+            glClearDepth(depth);
+            return this;
+        }
+
+        RenderPipeline* clearStencil(int stencil=0)
+        {
+            glClearStencil(stencil);
+            return this;
+        }
+
+        RenderPipeline* clear(GLbitfield mask=GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT)
+        {
+            glClear(mask);
+            return this;
+        }
+
+        RenderPipeline* colorMask(bool r, bool g, bool b, bool a)
+        {
+            glColorMask(r, g, b, a);
+            return this;
+        }
+
+        RenderPipeline* colorMask(bool color=true)
+        {
+            glColorMask(color, color, color, color);
+            return this;
+        }
+        
+        RenderPipeline* disableColorWriting()
+        {
+            return colorMask(false);
+        }
+
+        RenderPipeline* enableColorWriting()
+        {
+            return colorMask(true);
+        }
+
         glm::ivec2 _resolution;
         std::vector<std::shared_ptr<lithium::RenderGroup>> _renderGroups;
         std::vector<std::shared_ptr<lithium::RenderStage>> _renderStages;
