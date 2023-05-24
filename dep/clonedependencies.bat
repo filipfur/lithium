d9bfab4fb219a59f8f00c2e8df6594bb4a80f168
filipfur <filipfur@live.se>
2023-05-24 18:34:51 +0200

    now dependencies are submodules :s

--- dep/clonedependencies.bat
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/nlohmann/json.git
-git clone https://github.com/mackron/miniaudio.git
-git clone https://github.com/tinyobjloader/tinyobjloader.git
-git clone https://github.com/glfw/glfw
-git clone https://github.com/g-truc/glm.git
