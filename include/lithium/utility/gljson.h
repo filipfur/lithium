0ea594d52a45545d2a58adc0c4a73509205ff439
filipfur <filipfur@live.se>
2023-05-29 19:57:57 +0200

    working on json

--- include/lithium/utility/gljson.h
+++ include/lithium/utility/gljson.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
 
 namespace lithium::json
 {
@@ -11,7 +12,19 @@ namespace lithium::json
 
         virtual ~Object() noexcept;
     private:
-    };
+        void parse(std::istream& is, Object& obj);
 
-    std::istream& operator>>(std::istream& is, Object& obj);
+        friend std::istream& operator>>(std::istream& is, Object& obj);
+
+        Object(const Object& other) = delete;
+
+        Object(Object&& other) = delete;
+
+        Object& operator=(const Object& other) = delete;
+
+        Object& operator=(Object&& other) = delete;
+
+        Object* _parent{nullptr};
+        std::vector<Object> _children;
+    };
 }
--- src/gljson.cpp
+++ src/gljson.cpp
@@ -10,13 +10,100 @@ lithium::json::Object::~Object() noexcept
 
 }
 
-std::istream& lithium::json::operator>>(std::istream& is, lithium::json::Object& obj)
+void assertKey(const std::string& key)
+{
+    if(key.length() <= 0)
+    {
+        throw std::runtime_error("Key is empty.");        
+    }
+}
+
+void lithium::json::Object::parse(std::istream& is, lithium::json::Object& obj)
 {
     char c;
-    while(is >> c)
+    char startchar{0};
+    char endchar{0};
+    bool parseString{false};
+    std::string stringValue{""};
+
+    auto handleStart = [&](char start, char end) {
+        if(startchar == 0)
+        {
+            startchar = start;
+            endchar = end;
+        }
+        else
+        {
+            lithium::json::Object anotherOne;
+            anotherOne._parent = &obj;
+            obj.parse(is, anotherOne);
+            obj._children.push_back(anotherOne);
+        }
+    };
+
+    while((c = is.peek()) != EOF && !is.fail())
     {
-        std::cout << c;
-        std::cout << "hello" << std::endl;
+        //std::cout << c;
+        if(parseString)
+        {
+            if(c == '"')
+            {
+                parseString = false;
+                std::cout << "String: " << stringValue << std::endl;
+            }
+            else
+            {
+                stringValue += c;
+            }
+        }
+        else
+        {
+            switch(c)
+            {
+                case '{':
+                    handleStart(c, '}');
+                    break;
+                case '[':
+                    handleStart(c, ']');
+                    break;
+                case '"':
+                    parseString = true;
+                    break;
+                case ':':
+                    assertKey(stringValue);
+                    break;
+                case '}':
+                case ']':
+                    if(endchar != c)
+                    {
+                        throw std::runtime_error("Unexpected endchar.");
+                    }
+                    is.get();
+                    return;
+                    break;
+                case '\n':
+                case '\r':
+                case '\t':
+                case ' ':
+                    break;
+                default:
+                    throw std::runtime_error("Unexpected character.");
+                    break;
+            }
+        }
+        is.get();
+    }
+}
+
+std::istream& lithium::json::operator>>(std::istream& is, lithium::json::Object& obj)
+{
+    try
+    {
+        obj.parse(is, obj);
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
     }
     return is;
 }
